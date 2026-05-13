0daae63a74cbdd5a092490ea154c0c66f625dbbe
Matheus S Glanzmann <72605887+Glanzmann100@users.noreply.github.com>
2026-05-13 00:02:56 -0300

    first commit

---
+++ src/main/java/com/Glanzmann100/Projeto_de_Produtos/ProjetoDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.Glanzmann100.Projeto_de_Produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Glanzmann100/Projeto_de_Produtos/ProjetoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Glanzmann100.Projeto_de_Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
