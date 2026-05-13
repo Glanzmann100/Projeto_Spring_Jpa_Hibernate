6f458d124858d9a21d451aa52a519cb2651574ab
Matheus S Glanzmann <72605887+Glanzmann100@users.noreply.github.com>
2026-05-13 02:11:34 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/Glanzmann100/Projeto_de_Produtos/repositories/userRepository.java
@@ -0,0 +1,9 @@
+package com.Glanzmann100.Projeto_de_Produtos.repositories;
+
+import com.Glanzmann100.Projeto_de_Produtos.models.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface userRepository extends JpaRepository<user, Long> {
+}
